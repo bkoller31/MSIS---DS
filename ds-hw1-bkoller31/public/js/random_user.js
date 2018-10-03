28e3e404f51df582a76ddaca88c8527e11f0450d
bkoller31 <42586130+bkoller31@users.noreply.github.com>
2018-10-03 13:35:34 -0500

    homework update

--- ds-hw1-bkoller31/public/js/random_user.js
+++ ds-hw1-bkoller31/public/js/random_user.js
@@ -2,88 +2,38 @@ var randuserApp = new Vue ({
   el: '#randomUser',
   data:  {
     user_info: {
-      results: [
-    {
-      "gender": '',
-        "name": {
-        "title": '',
-        "first": '',
-        "last": ''
-      },
-      "location": {
-        "street": '',
-        "city": '',
-        "state": '',
-        "postcode": '',
-        "coordinates": {
-          "latitude": '',
-          "longitude": ''
-        },
-        "timezone": {
-          "offset": '',
-          "description": ''
-        }
-      },
-      "email": '',
-      "login": {
-        "uuid": '',
-        "username": '',
-        "password": '',
-        "salt": '',
-        "md5": '',
-        "sha1": '',
-        "sha256": ''
-      },
-      "dob": {
-        "date": '',
-        "age": ''
-      },
-      "registered": {
-        "date": '',
-        "age": ''
-      },
-      "phone": '',
-      "cell": '',
-      "id": {
-        "name": '',
-        "value": ''
-      },
-      "picture": {
-        "large": '',
-        "medium": '',
-        "thumbnail": ''
-      },
-      "nat": ''
-    }
-  ],
-  "info": {
-    "seed": '',
-    "results": '',
-    "page": '',
-    "version": ''
-    }
+        name:"",
+        location:"",
+        dob:"",
+        email:"",
+        picture:""
+
   }
 },
 
   methods: {
 
+    pretty_date: function(d){
+      return moment (d).format('l');
+    },
 
     fetchResults () {
       fetch('https://randomuser.me/api/')
       .then( response => response.json())
-      .then( json => {randuserApp.results = json})
+      .then( json => {randuserApp.user_info = json.results[0]})
       .catch ( err => {
         console.log('RESULTS FETCH ERROR:');
         console.log(err);
 
       })
+
     },
-    agenumber: function (){
+    agecalc: function (){
       return moment().diff(moment(this.user_info.dob.date),'years');
     },
   },
 
-  created () {
+  created: function () {
     this.fetchResults();
   }
 })
