984331e4e10560eb016fa9b6c0ede83231b5c961
bkoller31 <42586130+bkoller31@users.noreply.github.com>
2018-10-05 11:02:58 -0500

    video 10b catchup

--- public/api/work.php
+++ public/api/work.php
@@ -2,8 +2,15 @@
 
 require '../../app/common.php';
 
-$taskId = intval ($_GET['taskId'] ?? 0);
+$taskId = intval ($_GET['taskId'] ?? 0);  /*grab the integer value of task ID, but if it doesn't exist, use the value 0 instead */
 
 if ($taskId < 1) {
   throw new Exception ('Invalid Task ID')
 }
+
+// 1. Go to the database and get all work associated with the $taskId
+// 2. Convert to JSON
+// 3. Print
+
+/* This page is meant for the user to get a list of all the work
+completed for a particular task */
